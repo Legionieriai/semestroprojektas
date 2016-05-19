6b27bc9e5286e1dd402de7224dd403ddb6298dab
tomastomas844 <tomas.balasevicius@gmail.com>
2016-05-19 12:31:36 +0300

    v1.6

--- resources/views/aside.blade.php
+++ resources/views/aside.blade.php
@@ -1,3 +1,12 @@
 <aside>
-    <?php include 'C:\Users\Tomas\Desktop\Tutorials\Laravel5\resources\views\login.blade.php' ?>
+    <?php
+    //if(logged_in() === true)
+       // {
+        //    include 'C:\Users\Tomas\Desktop\Tutorials\Laravel5\resources\views\loggedin.blade.php';
+       // }else
+       // {
+            include 'C:\Users\Tomas\Desktop\Tutorials\Laravel5\resources\views\login.blade.php';
+      //  }
+//    include 'C:\Users\Tomas\Desktop\Tutorials\Laravel5\resources\views\user_count.blade.php';
+     ?>
 </aside>
---
+++ resources/views/user_count.blade.php
@@ -0,0 +1,12 @@
+{{--<div class="widget">--}}
+    {{--<h2> Users</h2>--}}
+    {{--<div class= "inner">--}}
+        {{--We current have x registered users--}}
+    {{--</div>--}}
+{{--</div>--}}
+{{--<?php--}}
+{{--$user_count = user_count();--}}
+{{--$suffix = ($user_count != 1) ? 's' : '';--}}
+{{--?>--}}
+{{--We current have <?php echo $user_count(); ?> registered user<?php echo $suffix;--}}
+{{--?>--}}
