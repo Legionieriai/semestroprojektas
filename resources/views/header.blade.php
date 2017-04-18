498577b657b6ffd9925f29941a51ac977b1620e8
tommyjlz <tommyjlz@users.noreply.github.com>
2017-04-18 16:14:33 +0100

    Delete header.blade.php

--- resources/views/header.blade.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <h1 class="logo">Lost And Found</h1>
-    <nav>
-        <u1>
-            <li><a href="index.blade.php">Home</a></li>
-        </u1>
-    </nav>
-    <div class="clear"></div>
-</header>
